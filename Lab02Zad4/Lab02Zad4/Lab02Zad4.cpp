aff49e3ed10d38f6033d022c49f804c173132b3f
Wiktor321 <wiktortomaszek@gmail.com>
2021-12-10 22:46:26 +0100

    Adds an empty C++ project
    Empty Code::Blocks project

---
+++ Lab02Zad4/Lab02Zad4/Lab02Zad4.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+	int liczba, Dodatnie = 0, Ujemne = 0;
+
+	for (int i = 1; i <= 10; i++) {
+
+		cout << "Wprowadz liczbe " << i << ": ";
+
+		cin >> liczba;
+
+		if (liczba >= 0) {
+
+			Dodatnie += liczba;
+
+		}
+
+		else {
+
+			Ujemne += liczba;
+
+		}
+	}
+	cout << endl;
+
+	cout << "Suma dodatnich: " << Dodatnie << endl;
+
+	cout << "Suma ujemnych: " << Ujemne << endl;
+
+}
